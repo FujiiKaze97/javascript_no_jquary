ba05ac9a3ff214f00746042ef421f78ebed39766
hongseungwoo <hongsw6934@gmail.com>
2024-08-01 09:40:02 +0900

    [fix]pagenation features css add & fix

--- js/movie.js
+++ js/movie.js
@@ -1,3 +1,7 @@
+// 현재 페이지 상태 관리
+let currentPage = 1;
+
+
 const cardData = (movie) => {
   const card = document.createElement('div');
   card.className = 'movie-card';
@@ -20,13 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
   if (searchInput) {
     searchInput.focus();
   }
-
   // 초기 데이터 로드
   loadMovies(currentPage);
+  // 페이지네이션 Number 처리
+  setPageNum();
+  addPageClickEvent();
+  isPrevNext();
 });
 
 // 키다운 이벤트
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.key === 'Enter') {
     document.getElementById('search-button').click();
   }
@@ -40,62 +47,127 @@ const options = {
   }
 };
 
-// 현재 페이지 상태 관리
-let currentPage = 1;
 
 // 영화 데이터를 로드하는 함수
 function loadMovies(page) {
-  fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}&region=KR`, options)
-    .then(response => response.json())
-    .then(data => {
-      const movies = data.results;
-      const movieContainer = document.getElementById('movie-container');
-      movieContainer.innerHTML = ''; // 이전 영화 목록 제거
 
-      movies.forEach(movie => {
-        const card = cardData(movie);
-        movieContainer.appendChild(card);
-      });
+  try {
+    fetch(`https://api.themoviedb.org/3/movie/popular?page=${page}&language=ko-KR&region=KR`, options)
+      .then(response => response.json())
+      .then(data => {
+        const movies = data.results;
+        const movieContainer = document.getElementById('movie-container');
+        movieContainer.innerHTML = ''; // 이전 영화 목록 제거
 
-      // 페이지네이션 상태 업데이트
-      document.getElementById('page-number').textContent = `Page: ${page}`;
-
-      // 이전 페이지 버튼 활성화/비활성화
-      const prevButton = document.getElementById('prev-page');
-      prevButton.disabled = page === 1;
-
-      // 다음 페이지 버튼 활성화/비활성화
-      const nextButton = document.getElementById('next-page');
-      nextButton.disabled = page === data.total_pages;
-
-    })
-    .catch(err => console.error(err));
+        movies.forEach(movie => {
+          const card = cardData(movie);
+          movieContainer.appendChild(card);
+        });
+      })
+      .catch(e => console.error(e));
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // 페이지네이션 버튼 이벤트 핸들러
 document.getElementById('prev-page').addEventListener('click', () => {
-  if (currentPage > 1) {
-    currentPage--;
-    loadMovies(currentPage);
+  try {
+    if (currentPage > 1) {
+      loadMovies(--currentPage);
+      setPageNum();
+      addPageClickEvent();
+      isPrevNext();
+    }
+  } catch (e) {
+    console.log(e);
   }
 });
 
 document.getElementById('next-page').addEventListener('click', () => {
-  currentPage++;
-  loadMovies(currentPage);
+  try {
+    loadMovies(++currentPage);
+    setPageNum();
+    addPageClickEvent();
+    isPrevNext();
+  } catch (e) {
+    console.log(e);
+  }
+
 });
 
 // 검색 버튼 이벤트 핸들러
 document.getElementById('search-button').addEventListener('click', () => {
-  // 무조건 소문자로 처리하여.. 대소문자 상관없이..
-  const query = document.getElementById('search-input').value.toLowerCase();
-  const movieCards = document.querySelectorAll('.movie-card');
-  movieCards.forEach(card => {
-    const title = card.querySelector('h3').textContent.toLowerCase();
-    if (title.includes(query)) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  });
+  try {
+    // 무조건 소문자로 처리하여.. 대소문자 상관없이..
+    const query = document.getElementById('search-input').value.toLowerCase();
+    const movieCards = document.querySelectorAll('.movie-card');
+    movieCards.forEach(card => {
+      const title = card.querySelector('h3').textContent.toLowerCase();
+      if (title.includes(query)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
+
+// Feature : MainPage Pagenation 
+function setPageNum() {
+  try {
+    const numberButtonWrapper = document.querySelector('.number-button-wrapper');
+    numberButtonWrapper.innerHTML = ''; // 페이지 번호 wrapper 내부를 비워줌
+    
+    if (currentPage >= 6) {
+      for (let i = currentPage - 5; i < currentPage + 5; i++) {
+        numberButtonWrapper.innerHTML += `<span class="number-button ${currentPage === i ? 'selected' : ''}"> ${i} </span`;
+      }
+    } else {
+      for (let i = 1; i <= 10; i++) {
+        numberButtonWrapper.innerHTML += `<span class="number-button ${currentPage === i ? 'selected' : ''}"> ${i} </span`;
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+};
+
+  // 페이지별 클릭 이벤트 
+function addPageClickEvent() {
+
+  try {
+    const pageNumberButtons = document.querySelectorAll('.number-button');
+
+    pageNumberButtons.forEach((numberButton) => {
+      numberButton.addEventListener('click', (e) => {
+        currentPage = +e.target.innerHTML;
+        loadMovies(currentPage);
+        setPageNum();
+        addPageClickEvent();
+        isPrevNext();
+      });
+    });
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// 이전, 다음 버튼이 최솟값 , 최댓값일 때는 활성화 되지 않도록 
+function isPrevNext() {
+  try {
+    // 이전 페이지 버튼 활성화/비활성화
+    const prevButton = document.getElementById('prev-page');
+    prevButton.disabled = currentPage === 1;
+
+    // 다음 페이지 버튼 활성화/비활성화
+    const nextButton = document.getElementById('next-page');
+    nextButton.disabled = currentPage === 500;
+  } catch (e) {
+    console.log(e);
+  }
+}
