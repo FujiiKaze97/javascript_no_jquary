162e8bcec60eca771c3e89f3013e832148d9a32c
joycie416 <joycie416@gmail.com>
2024-08-01 15:13:40 +0900

    [add] create modal window files

--- js/createReview.js
+++ js/createReview.js
@@ -1,18 +1,20 @@
 // '리뷰 작성' 버튼을 클릭하면 모달창이 뜨도록 함
-const creatReviewBtn = document.getElementById('show_modal_btn');
+const creatReviewBtn = document.getElementById('write_review_btn');
 creatReviewBtn.addEventListener('click', () => {
   showModal();
 })
 const showModal = () => {
-  console.log('"create review button" clicked');
-  
+  console.log('"write_review_btn" clicked');
+
   const modal = document.getElementsByClassName('modal_create_review')[0];
   modal.style.display = 'flex';
+
+  // document.body.style.overflow = 'hidden';
+  
 }
 
 
 // 취소 버튼을 누르면 모달창이 안보이도록 함.
-// textarea의 내용도 자동으로 지워짐.
 const cancelBtn = document.getElementById('cancel_review_btn');
 cancelBtn.addEventListener('click', () => {
   cancelReview();
@@ -20,11 +22,25 @@ cancelBtn.addEventListener('click', () => {
 
 const cancelReview = () => {
   console.log("'cancel review button' clicked");
-  alert('리뷰 작성을 취소하셨습니다.')
-  
-  const modal = document.getElementsByClassName('modal_create_review')[0];
-  modal.style.display = 'none';
-  
+  const result = confirm('리뷰 작성을 취소하시겠습니까?');
+  if (result) {
+    const name = document.getElementById('name');
+    const pw = document.getElementById('pw');
+    const review = document.getElementById('review_text');
+    const score = document.getElementById('select_star');
+
+    // reset : 기존 모달창에 있던 내용 초기화
+    // 새로고침은 하지 않음
+    clearModal(name, pw, review, score);
+    const modal = document.getElementsByClassName('modal_create_review')[0];
+    modal.style.display = 'none';
+  }
+  // else {
+  // const modal = document.getElementsByClassName('modal_create_review')[0];
+  // modal.style.display = 'none';
+  // }
+
+
 }
 
 // 등록 버튼을 누르면 log만 찍히도록 함.
@@ -36,17 +52,57 @@ createBtn.addEventListener('click', () => {
 
 const createReview = () => {
   console.log("'create review button' clicked");
-  alert('리뷰를 등록했습니다.');
+  //등록 버튼 클릭시 컨펌 알림
+  const result = confirm('등록하시겠습니까?');
 
 
-  const content = document.getElementById('review_text').value;
-  const name = document.getElementById('name').value;
-  const pw = document.getElementById('pw').value;
+  const name = document.getElementById('name');
+  const pw = document.getElementById('pw');
+  const review = document.getElementById('review_text');
+  const score = document.getElementById('select_star');
 
-  console.log(name, pw);
-  console.log('content :', content);
+  // console.log(name.value, pw.value);
+  // console.log('score :', score.value)
+  // console.log('review :', review.value);
 
-  
-  const modal = document.getElementsByClassName('modal_create_review')[0];
-  modal.style.display = 'none';
+  const reviewInfo = { name: name.value, pw: pw.value, score: score.value, review: review.value };
+  if (result) {
+    //컨펌 확인 버튼 입력시 처리
+    if (localStorage.getItem('test')) {
+      //로컬 스토리지에 reviews 가 있을경우 내용 추가
+      let pushReview = getReview('reviews');
+      pushReview.push(reviewInfo);
+      localStorage.setItem('reviews', JSON.stringify(pushReview));
+
+
+    } else {
+      //로컬 스토리지에 reviews가 없을 경우 새로운 배열 저장
+      saveReview([reviewInfo]);
+
+    }
+    // 저장 후 페이지 새로고침
+    location.reload();
+  }
+  // confirm 창에서 취소를 누르면 위에 주석처리한 부분 log가 또 뜨네요?
+
 }
+
+// localStorage에 저장하기
+const saveReview = (info) => {
+  localStorage.setItem('test', JSON.stringify(info));
+}
+// localStorage 불러오기
+const getReview = (key) => {
+  let localData = JSON.parse(localStorage.getItem(key))
+  return localData;
+}
+
+const clearModal = (name, pw, review, score) => {
+  // reset : 기존 모달창에 있던 내용 초기화
+  name.value = '';
+  pw.value = '';
+  review.value = '';
+  score.value = '⭐⭐⭐⭐⭐';
+}
+
+
