4391b61bb0490cd38a49011ec2cb4f50fc5731a3
unknown <tjrtkd95@gmail.com>
2024-08-06 16:12:30 +0900

    fix: 다크모드 토글 clss,id 수정

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,13 +1,13 @@
 // DOM 로딩시 로컬스토리지 테마 적용
 document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle'); // 토글버튼
+    const themeToggle = document.getElementById('theme_toggle'); // 토글버튼
     const savedTheme = localStorage.getItem('theme') || 'light'; // 로컬스토리지 테마 불러오기
     const isDarkMode = savedTheme === 'dark'; // 다크모드 boolean
 
     // 토글버튼 이벤트
     themeToggle.addEventListener('click', () => {
         // body태그에 class 토글 / 다크모드 경우 true 반환
-        const isDarkMode = document.body.classList.toggle('dark-mode');
+        const isDarkMode = document.body.classList.toggle('dark_mode');
         // 로컬스토리지 테마 저장 true=>'dark' false=>'light'
         localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
         applyTheme(isDarkMode);
@@ -21,13 +21,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // 테마 적용 함수
 function applyTheme(isDarkMode) {
-    const darkModeStylesheet = document.getElementById('darkMode-stylesheet');
+    const darkModeStylesheet = document.getElementById('darkMode_stylesheet');
 
     if (isDarkMode) {
         // 저장 테마 dark 일 경우
         if (!darkModeStylesheet) {
             const link = document.createElement('link');
-            link.id = 'darkMode-stylesheet';
+            link.id = 'darkMode_stylesheet';
             link.rel = 'stylesheet';
             link.href = './css/darkMode.css';
             // head 에 다크모드 link 태그 추가하기
